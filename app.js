c13902740a11cc1e5602069a0895b11508b5a0b7
FCraven <craven.francis@gmail.com>
2018-11-28 21:00:07 -0500

    slash route returns layout.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express();
 const morgan = require('morgan')
+const layout = require('./views/layout')
 
 app.use(morgan('dev'));
 app.use(express.static('public'))
-app.use(express.urlencoded({extended :true}))
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req,res) => {
-  res.send('Hello World')
+  res.send(layout(''))
 })
 
 
