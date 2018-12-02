c8dcdaf2e84c24d87590fc8d4e7a7446129a77d4
FCraven <craven.francis@gmail.com>
2018-12-01 21:32:46 -0500

    routes added on /wiki

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const wikiRouter = express.Router()
 
+wikiRouter.get('/', (req,res,next) => {
+  res.send('Landed on GET route for all wiki pages')
+})
 
+wikiRouter.post('/',(req,res,next) => {
+  res.send('You hit the POST route for /wiki')
+})
+
+wikiRouter.get('/add', (req,res,next) => {
+  res.send(`You've hit the route to retrieve the 'Add a Page' form` )
+})
 
 module.exports = wikiRouter
