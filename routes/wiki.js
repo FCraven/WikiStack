7c129e5959756acf60ccfc3f4e03f74129a605bb
FCraven <craven.francis@gmail.com>
2018-12-03 11:29:42 -0500

    get route added for /:slug New route tested, returns info from db

--- routes/wiki.js
+++ routes/wiki.js
@@ -8,23 +8,37 @@ wikiRouter.get('/', (req,res,next) => {
   res.redirect('/wiki')
 })
 
-wikiRouter.post('/', async (req,res,next) => {
- const page = new Page({
-   title: req.body.title,
-   content: req.body.content
- });
 
- try{
-   await page.save();
-   console.log(page)
-   res.redirect('/')
- } catch (error) {
-   next(error)
- }
+wikiRouter.post('/', async (req,res,next) => {
+  const page = new Page({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  try{
+    await page.save();
+    console.log(page)
+    res.redirect('/')
+  } catch (error) {
+    next(error)
+  }
 })
 
 wikiRouter.get('/add', (req,res,next) => {
   res.send(addPage())
+})
+
+wikiRouter.get('/:slug', async (req,res,next) => {
+  try {
+    const page = await Page.findOne({
+      where: {
+        slug: req.params.slug
+      }
+    })
+    res.json(page)
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = wikiRouter
