0eb4c3dda848376d75cccf3fe96ba5de56a7fef0
FCraven <craven.francis@gmail.com>
2018-12-03 20:43:20 -0500

    pages able to be updated and deleted

--- routes/wiki.js
+++ routes/wiki.js
@@ -4,6 +4,7 @@ const addPage = require('../views/addPage')
 const { Page, User } = require("../models")
 const wikipage = require('../views/wikipage')
 const main = require('../views/main')
+const editPage = require('../views/editPage')
 
 
 
@@ -46,7 +47,8 @@ wikiRouter.get('/:slug', async (req,res,next) => {
     })
 
     if(!page){
-      res.status(404).send('That page no live here no more. GO AWAY!!')
+      res.status(404)
+          .redirect('/wiki')
     } else {
       const author = await page.getAuthor()
       res.send(wikipage(page,author))
@@ -55,5 +57,65 @@ wikiRouter.get('/:slug', async (req,res,next) => {
     next(error)
   }
 })
+
+wikiRouter.get('/:slug/edit', async (req,res,next) => {
+  try {
+    const page = await Page.findOne({
+      where: {
+        slug: req.params.slug
+      }
+    })
+    if(!page){
+      res.status(404)
+          .redirect(`/wiki/${req.params.slug}`)
+    } else {
+      const author = await page.getAuthor()
+      res.send(editPage(page, author))
+    }
+  } catch (error) {
+      next(error)
+    }
+})
+
+wikiRouter.post('/:slug', async (req,res,next) => {
+  try {
+    console.log(req.body)
+    const page = await Page.findOne({
+      where: {
+        slug: req.params.slug
+      }
+    });
+
+    if(!page) {
+      res.status(404).redirect(`/wiki/${req.params.slug}`)
+    } else {
+        await page.update({
+          name: req.body.name,
+          email: req.body.email,
+          title: req.body.title,
+          content: req.body.content,
+          status: req.body.status
+        })
+      res.redirect(`/wiki/${req.params.slug}`)
+    }
+  } catch (error) {
+      next(error)
+    }
+  })
+
+wikiRouter.get('/:slug/delete', async (req,res,next) => {
+  try{
+    await Page.destroy({
+      where: {
+        slug: req.params.slug
+      }
+    })
+    res.redirect('/wiki')
+  } catch (error) {
+      next (error)
+  }
+})
+
+
 
 module.exports = wikiRouter
